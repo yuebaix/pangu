be5534cb6a8fab13e741cc6590acdabf270bf9f3
yuebaix <yuebaix@outlook.com>
2021-09-04 19:35:57 +0800

    💻 add property source init log

--- pangu-spring-boot-starter/src/main/java/com/yuebaix/pangu/config/PanGuPropertySourceContextInitializer.java
+++ pangu-spring-boot-starter/src/main/java/com/yuebaix/pangu/config/PanGuPropertySourceContextInitializer.java
@@ -1,15 +1,18 @@
 package com.yuebaix.pangu.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
+@Slf4j
 public class PanGuPropertySourceContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
     @Override
     public void initialize(ConfigurableApplicationContext context) {
         ConfigurableEnvironment environment = context.getEnvironment();
         if (!environment.getPropertySources().contains(PanGuPropertySource.PROPERTY_SOURCE_NAME)) {
             environment.getPropertySources().addLast(new PanGuPropertySource());
+            log.info(this.getClass().getSimpleName() + "--->PropertySource Initialized");
         }
     }
 }
