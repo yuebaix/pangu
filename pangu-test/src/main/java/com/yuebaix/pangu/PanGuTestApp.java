6a46e88be0394bb003cbe56cdc3f23b2e709d72d
yuebaix <yuebaix@outlook.com>
2021-09-04 18:06:34 +0800

    ✨init frame

---
+++ pangu-test/src/main/java/com/yuebaix/pangu/PanGuTestApp.java
@@ -0,0 +1,11 @@
+package com.yuebaix.pangu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanGuTestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PanGuTestApp.class, args);
+    }
+}
